b510c662cdba122b1722967c23cfd601d44e2ec0
chadah <70854062+tito-arch@users.noreply.github.com>
2024-11-18 13:41:00 +0300

    Update iframe source URL to point to the production environment

--- interactiveIframeEmbed.js
+++ interactiveIframeEmbed.js
@@ -87,7 +87,7 @@
     // Create the iframe
     const iframe = document.createElement("iframe");
     iframe.id = "custom-iframe";
-    iframe.src = "http://localhost:3001/home";
+    iframe.src = "http://interactive.proximaai.co/home";
 
     // Append elements
     iframeContainer.appendChild(closeButton);
